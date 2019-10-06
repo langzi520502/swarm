c1fd5890849af43d982a0a03de73394add6ef9be
langzi <673263353@qq.com>
2019-10-06 15:00:28 +0800

    swarm

--- src/main/java/com/example/swarm/controller/Hello.java
+++ src/main/java/com/example/swarm/controller/Hello.java
@@ -1,9 +1,9 @@
-package controller;
+package com.example.swarm.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @ResponseBody
@@ -12,6 +12,6 @@ public class Hello {
     @RequestMapping("/hello")
     public String hello(){
 
-        return "langzi swarm!";
+        return "hello swarm";
     }
 }
