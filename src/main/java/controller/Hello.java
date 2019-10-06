7cbbfc1c94a66191ad014f1a9f7d3ef0e32b24c4
langzi <673263353@qq.com>
2019-10-06 13:32:54 +0800

    change code

--- src/main/java/controller/Hello.java
+++ src/main/java/controller/Hello.java
@@ -1,14 +1,15 @@
 package controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ResponseBody
 public class Hello {
 
-    @ResponseBody
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String hello(){
 
         return "langzi swarm!";
